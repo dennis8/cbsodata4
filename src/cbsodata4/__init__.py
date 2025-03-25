5d6aaefebfc83fa1c07b41dbf89b2d90b557a13f
dennis-albers <Dennis.Albers@ortec-finance.com>
2025-03-25 23:59:23 +0100

    Add versioning in __init__.py

--- src/cbsodata4/__init__.py
+++ src/cbsodata4/__init__.py
@@ -1,13 +1,15 @@
 from .catalogs import get_catalogs
 from .data_processor import get_wide_data
+from .dataset_search import search_datasets
 from .datasets import get_datasets
 from .date_handler import add_date_column
 from .downloader import download_dataset
 from .labeler import add_label_columns
 from .metadata import get_metadata
 from .observations import get_observations
-from .dataset_search import search_datasets
 from .unit_handler import add_unit_column
+
+__version__ = "0.1.1"
 
 __all__ = [
     "get_catalogs",
